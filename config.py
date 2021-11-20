05f3c09c48beaab4bf9bf2c0a474bc4be1791a4d
lucasernesto <lcspuppimernesto@gmail.com>
2021-11-20 12:26:35 -0300

    Ajustar erro de criar db e atualizar requirements

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 DEBUG = True
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///storage.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'storage.db')
 DEBUG = True
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
